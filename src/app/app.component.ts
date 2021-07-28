161756833de7d2e23f60bb1e922e501aca104fa8
weetheng <weetheng11@gmail.com>
2021-07-28 21:34:43 +0800

    first code change
    rough layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'CalculatorAngular';
+export class AppComponent implements OnInit{
+  displayValue: number;
+  tempValue: number;
+  deletePressed: boolean;
+  functionPressed: boolean;
+  function:string;
+  numbersPressed:boolean;
+
+  ngOnInit(){
+    this.displayValue = 0;
+    this.tempValue = 0;
+    this.deletePressed = false;
+    this.functionPressed = false;
+    this.function = "";
+  }
+  deleteDisplayValue(){
+    this.displayValue = 0;
+  }
+  resetValues(){
+    this.displayValue = 0;
+    this.tempValue = 0;
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
 
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
